c5f474795b419c5f1d6ba423e2801381e8172257
peijung tsai <ptsa0008@student.monash.edu>
2025-08-21 13:16:43 +1000

    style by css

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,15 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import JSON from './components/JSON.vue'
-import Form from './components/ Form.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+    <!--<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div>-->
   </header>
 
   <main>
